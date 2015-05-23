609c0894279ae2f801a2d32c4ac4cfcccf728372
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2015-05-23 12:10:15 +0000

    Client now receives chats with the usernames.

--- project/src/server.h
+++ project/src/server.h
@@ -3,14 +3,14 @@
 
 #include "messages.pb-c.h"
 
-#define FIFO_NAME_BROADCAST "/tmp/server"	// a PID will be appended
+#define FIFO_NAME_BROADCAST "/tmp/server-broadcast"	// a PID will be appended
 
 void server(void);
 void * incoming_connection(void *fd);
 void * broadcast_chat(void *arg);
-int manage_login(int fd, ClientToServer *msg, int loggedin);
+int manage_login(int fd, ClientToServer *msg, int loggedin, char **username);
 void manage_disconnect(int fd, int loggedin);
 void manage_query(int fd, ClientToServer *msg, int loggedin);
-void manage_chat(int fd, ClientToServer *msg, int loggedin);
+void manage_chat(int fd, ClientToServer *msg, int loggedin, char *username);
 
 #endif
